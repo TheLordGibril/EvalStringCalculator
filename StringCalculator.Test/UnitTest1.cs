9b8916e79c7336c5c139b625a3974e91fb7d5d67
TheLordGibril <gabidule03@gmail.com>
2024-03-07 10:20:52 +0100

    Nouveau test rouge : plusieurs nombres en entrée.

--- StringCalculator.Test/UnitTest1.cs
+++ StringCalculator.Test/UnitTest1.cs
@@ -13,9 +13,35 @@ namespace StringCalculator.Test
 
             var str = $"{nombreA},{nombreB}";
 
-            var result = StringCalculator.Parse(str);
+            var resultat = StringCalculator.Parse(str);
 
-            Assert.Equal(nombreA+nombreB, result);
+            Assert.Equal(nombreA+nombreB, resultat);
+        }
+
+        [Fact]
+
+        public void PlusieursNombresEnEntree()
+        {
+            var random = Random.Shared;
+
+            var length = random.Next(1,21);
+
+            var suiteNumerique = new int[length];
+
+            var attendu = 0;
+
+            for (var i = 0; i < length; i++)
+            {
+                var nombreAleatoire = random.Next(1,101);
+                suiteNumerique[i] = nombreAleatoire;
+                attendu += nombreAleatoire;
+            }
+
+            var suiteFormatee = string.Join(",", suiteNumerique);
+
+            var resultat=StringCalculator.Parse(suiteFormatee);
+
+            Assert.Equal(attendu, resultat);
         }
     }
 }
